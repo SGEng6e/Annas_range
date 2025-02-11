99370257914361c873f1c397052ed749d49cf5fa
SGEng6e <64223678+SGEng6e@users.noreply.github.com>
2025-02-11 18:56:37 -0500

    initial commit

---
+++ figures.R
@@ -0,0 +1,256 @@
+# Visualization code (Figures 1 and S1) for:
+# Supplemental feeding as a driver of population expansion and morphological change in Anna’s Hummingbirds
+
+# NM Alexandre, FG Romero, SG English, E Grames, F Garzón-Agudelo, K Epperly, T Barnes, DR Powers, 
+## AE Smith, Z Migicovsky, L Stein, S Akalu, H Sridhar, G Montross, E Collins, A Rico-Guevara
+
+##### LIBRARIES #############################################################################################
+
+library(tidyverse)
+library(terra)
+library(tidyterra)
+library(MCMCvis)
+library(coda)
+library(nimble)
+library(ggpubr)
+library(magick)
+library(gridExtra)
+
+##### LOAD DATA #############################################################################################
+
+load("./nimble_data.rda")
+load("./nimble_constants.rda")
+
+load("./anna_glmnuts_chain1.rda")
+load("./anna_glmnuts_chain2.rda")
+load("./anna_glmnuts_chain3.rda")
+load("./anna_glmnuts_chain4.rda")
+annas_chains <- mcmc.list(mget(ls(pattern = "chain\\d"))) ; rm(list = ls(pattern = "chain\\d"))
+
+##### DATA SUMMARY ##########################################################################################
+
+quants <- c(0.05,0.95)
+
+marginals <- set_names(c("baseline","marginal_fa","marginal_ea","marginal_hp","marginal_yr"),
+                       c("baseline","fa","^ea","hp","year"))
+
+raw_dat <- imap(marginals,
+                \(x,y) {
+                  if (x == "baseline") {
+                    tibble(
+                      Term = paste0("baseline[",nimble.data$latlon_raw[,1],"]"),
+                      route = nimble.data$latlon_raw[,1],
+                      lat = nimble.data$latlon_raw[,2],
+                      lon = nimble.data$latlon_raw[,3],
+                      raw = NA)
+                  } else {
+                    dat <- unlist(nimble.data[which(str_detect(names(nimble.data), y))])
+                    tibble(
+                      Term = paste0(x,"[",1:length(dat), "]"),
+                      route = nimble.constants$route,
+                      year = nimble.data$year_raw,
+                      lat = nimble.data$Latitude,
+                      lon = nimble.data$Longitude,
+                      raw = dat)
+                  }
+                }) %>% 
+  bind_rows()
+
+main_result_summary <- MCMCsummary(
+  map(annas_chains, \(x) x[,which(!str_detect(colnames(x), "(N)|(marginal)|(baseline)|(ltln)"))]),
+  probs = quants, pg0 = T, Rhat = T) %>% 
+  rownames_to_column("Term") %>% 
+  mutate(Term_name = factor(
+    Term, levels = rev(c("beta_ea","beta_fa","beta_hp","beta_yr","beta_lt","beta_in","beta_ef","thet_ef",
+                         "sd_ovds","Rn1","Rn2","bayes.p")),
+    labels = rev(c("Eucalyptus\navailability","Feeder\navailability","Human\npopulation","Year",
+                   "Year-by-\nlatitude","Abundance\nintercept","Effort\nslope","Effort\nsaturation",
+                   "Overdispersion","Observed\nError","Simulated\nError","Bayesian\nP-value"))),
+    signif = factor(case_when(`p>0` > 0.95 ~ 1,
+                              `p>0` < 0.05 ~ -1,
+                              `p>0` > 0.9 ~ 0.5,
+                              `p>0` < 0.1 ~ -0.5,
+                              between(`p>0`, 0.1, 0.9) ~ 0),
+                    levels = paste(seq(-1,1, by = 0.5))))
+
+GAM_component_summary <- MCMCsummary(
+  map(annas_chains, \(x) x[,which(str_detect(colnames(x),"ltln"))]),
+  probs = quants, pg0 = T, Rhat = T) %>% 
+  rownames_to_column("Term")
+
+marginal_effects_summary <- MCMCsummary(
+  map(annas_chains, \(x) x[,which(str_detect(colnames(x), "(marginal)|(baseline)"))]),
+  probs = quants, pg0 = T, Rhat = T) %>% 
+  rownames_to_column("Term") %>% 
+  left_join(raw_dat, by = "Term") %>% 
+  mutate(Term_id = str_extract(Term, "(?<=\\[)\\d{1,4}(?=\\])"),
+         Term = str_extract(Term, ".*(?=\\[)")) %>% 
+  distinct(Term, lon, lat, route, raw, year, .keep_all = T) %>% 
+  mutate(Term_name = factor(
+    Term, levels = rev(c("baseline","marginal_ea","marginal_fa","marginal_hp","marginal_yr")),
+    labels = rev(c("Baseline","Eucalyptus","Feeders","Human\npopulation","Year")))) %>% 
+  relocate(lon, lat, .before = 1) %>% 
+  pivot_wider(id_cols = c(lat,lon,route,year), names_from = Term_name, values_from = c(raw,mean,sd)) %>% 
+  relocate(lat, lon, route, year,
+           contains("Baseline"), contains("Feeders"), contains("Eucalyptus"), contains("Human\npopulation"))
+
+marginal_change_map <- marginal_effects_summary %>% 
+  filter(!is.na(year)) %>% 
+  group_by(route, lon, lat) %>% 
+  filter(year == min(year) | year == max(year)) %>% 
+  filter(n() > 1) %>% 
+  summarise(Min_year = min(year),
+            Max_year = max(year),
+            across(matches("(mean_)"), \(x) { ((x[which.max(year)]/x[which.min(year)])-1)*100 }, #|(raw_)
+                   .names = "pct_change_{col}"),
+            .groups = "drop") %>% 
+  rename_with(\(x) str_remove(x, "mean_")) %>% 
+  select(-contains("Baseline"))
+
+##### MARGINAL MAPS #########################################################################################
+
+calif <- vect(
+  map_data("state") %>% 
+    filter(region == "california") %>% 
+    select(lon = long, lat) %>% 
+    as.matrix(), 
+  type= "polygon", crs="+proj=longlat")
+
+base_df <- marginal_effects_summary %>% 
+  filter(!is.na(mean_Baseline)) %>% 
+  select(1:3,6,7)
+
+baseline_map <- rast(nrow = length(unique(base_df$lat)),
+                     ncol = length(unique(base_df$lon)),
+                     xmin = min(base_df$lon), xmax = max(base_df$lon),
+                     ymin = min(base_df$lat), ymax = max(base_df$lat),
+                     crs = "EPSG:4326", resolution = 0.2) %>% 
+  rasterize(vect(base_df), ., field = names(base_df)[-c(1:2)])
+
+change_map <- rast(nrow = length(unique(marginal_change_map$lat)),
+                   ncol = length(unique(marginal_change_map$lon)),
+                   xmin = min(marginal_change_map$lon), xmax = max(marginal_change_map$lon),
+                   ymin = min(marginal_change_map$lat), ymax = max(marginal_change_map$lat),
+                   crs = "EPSG:4326", resolution = 0.2) %>% 
+  rasterize(vect(marginal_change_map), ., field = names(marginal_change_map)[-c(2,3)]) %>% 
+  select(-contains("Baseline"), -contains("raw")) %>% 
+  rename_with(\(x) str_replace_all(x, c("Feeders"="fa","Eucalyptus"="ea","Human\npopulation"="pop","Year"="yr")))
+
+counties <- map_data("county") %>% 
+  filter(region == "california") %>% 
+  group_by(subregion) %>% 
+  group_map(\(x,y) {
+    cnty <- x %>% 
+      select(lon = long, lat) %>% 
+      as.matrix()
+    vect(cnty, type= "polygon", crs="+proj=longlat", atts = y)
+  }) %>% 
+  bind_spat_rows()
+
+##### VISUALIZATION #########################################################################################
+
+thm_obj <- theme(
+  axis.text = element_text(size = 11),
+  panel.background = element_blank(),
+  strip.text = element_text(size = 12),
+  axis.line = element_line(colour = "black"),
+  legend.title = element_text(vjust = 2.5, size = 13, hjust = 1),
+  legend.text = element_text(face = "bold"),
+  legend.background = element_blank()
+)
+
+# check main effects, excluding intercept, dispersion, smoothing parameters
+ggplot(filter(main_result_summary, !str_detect(Term, "(sd)|(_in)|(_ef)|(Rn)|(bayes)")),
+       aes(x = Term_name, y = mean, colour = signif))+
+  scale_y_continuous(expand = c(0,0.1))+
+  geom_hline(yintercept = 0, lty = 2, colour = "darkgrey")+
+  geom_point(size = 2)+
+  # crossbars correspond to 90% credible intervals
+  geom_linerange(aes(ymin = `5%`, ymax = `95%`), linewidth = 1)+
+  scale_colour_manual(values = c("#a50026","#d73027","black","#4575b4","#313695"), drop = F)+
+  guides(colour = "none")+
+  labs(y = "Effect sizes on expected counts\nof Anna's hummingbirds", x = "", fill = "")+
+  coord_flip()+
+  thm_obj+
+  theme(axis.text = element_text(face = "bold"),
+        axis.title = element_text(size = 13, face = "bold"))
+
+##### PERCENT CHANGE IN MARGINAL EFFECTS
+lt_map <- ggplot()+
+  geom_spatvector(data = counties, fill = "#FFFFF2")+
+  geom_spatraster(data = filter(change_map, Min_year < 1980, Max_year > 2015),
+                  aes(fill = pct_change_yr), na.rm = T)+
+  scale_fill_viridis_c(na.value = "transparent", 
+                       name = "Change in expected\ncount due to marginal\nyear-by-latitude effect",
+                       breaks = seq(0,150,50), labels = paste0(seq(0,150,50),"%"), limits = c(0,175))+
+  thm_obj+
+  # annotation_raster(as.raster(image_read("./yr.png")), -123.25, -121, 33.5, 34.9)+
+  annotate("text", x = -124, y = 33, label = "bold(A)", parse = T, size = 8)+
+  theme(legend.position = "inside",
+        legend.position.inside = c(.8, .75),
+        axis.text.y = element_text(size = 13),
+        axis.title = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.line.x = element_blank(),
+        panel.spacing = unit(0, "cm"),
+        plot.margin = margin(t=0, r=0, b=0, l=0, unit = "mm"))
+
+ea_map <- ggplot()+
+  geom_spatvector(data = counties, fill = "#FFFFF2")+
+  geom_spatraster(data = filter(change_map, Min_year < 1980, Max_year > 2015),
+                  aes(fill = pct_change_ea), na.rm = T)+
+  scale_fill_viridis_c(na.value = "transparent", 
+                       name = "Change in expected\ncount from change in\neucalytpus availability",
+                       breaks = seq(0,1.5,0.3), labels = paste0(seq(0,1.5,0.3),"%"), limits = c(0,1.5))+
+  thm_obj+
+  # annotation_raster(as.raster(image_read("./ea.png")), -123.25, -121.25, 33.25, 34.75)+
+  annotate("text", x = -124, y = 33, label = "bold(D)", parse = T, size = 8)+
+  theme(legend.position = "inside",
+        legend.position.inside = c(.8, .75),
+        axis.title = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.line.y = element_blank(),
+        axis.text.x = element_text(size = 13, hjust = 2/3),
+        panel.spacing = unit(0, "cm"),
+        plot.margin = margin(t=0, r=2, b=0, l=-10, unit = "mm"))
+
+fa_map <- ggplot()+
+  geom_spatvector(data = counties, fill = "#FFFFF2")+
+  geom_spatraster(data = filter(change_map, Min_year < 1980, Max_year > 2015),
+                  aes(fill = pct_change_fa), na.rm = T)+
+  scale_fill_viridis_c(na.value = "transparent", 
+                       name = "Change in expected\ncount from change\nin feeder availability",
+                       breaks = seq(0,125,25), labels = paste0(seq(0,125,25),"%"), limits = c(0,125))+
+  thm_obj+
+  annotate("text", x = -124, y = 33, label = "bold(C)", parse = T, size = 8)+
+  # annotation_raster(as.raster(image_read("./fa.png")), -123.25, -121.25, 33.25, 35.25)+
+  theme(legend.position = "inside",
+        legend.position.inside = c(.8, .75),
+        axis.title = element_blank(),
+        axis.text.y = element_text(size = 13),
+        axis.text.x = element_text(size = 13, hjust = 2/3),
+        panel.spacing = unit(0, "cm"),
+        plot.margin = margin(t=0, r=2, b=0, l=-10, unit = "mm"))
+
+hp_map <- ggplot()+
+  geom_spatvector(data = counties, fill = "#FFFFF2")+
+  geom_spatraster(data = filter(change_map, Min_year < 1980, Max_year > 2015),
+                  aes(fill = pct_change_pop), na.rm = T)+
+  scale_fill_viridis_c(na.value = "transparent", 
+                       name = "Change in expected\ncount from change\nin human population",
+                       breaks = seq(0,60,15), labels = paste0(seq(0,60,15),"%"), limits = c(0,60))+
+  thm_obj+
+  annotate("text", x = -124, y = 33, label = "bold(B)", parse = T, size = 8)+
+  # annotation_raster(as.raster(image_read("./hp.png")), -123.25, -121.25, 33.35, 35.25)+
+  theme(legend.position = "inside",
+        legend.position.inside = c(.8, .75),
+        axis.title = element_blank(),
+        axis.line = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        panel.spacing = unit(0, "cm"),
+        plot.margin = margin(t=0, r=0, b=0, l=0, unit = "mm"))
+
+ggarrange(plotlist = list(lt_map,hp_map,fa_map,ea_map), ncol = 2, nrow = 2, align = "hv")
---
+++ glmnuts.R
@@ -0,0 +1,123 @@
+
+# Nimble model code for:
+# Supplemental feeding as a driver of population expansion and morphological change in Anna’s Hummingbirds
+
+# NM Alexandre, FG Romero, SG English, E Grames, F Garzón-Agudelo, K Epperly, T Barnes, DR Powers, 
+## AE Smith, Z Migicovsky, L Stein, S Akalu, H Sridhar, G Montross, E Collins, A Rico-Guevara
+
+##### LIBRARIES #############################################################################################
+
+library(nimble)
+library(nimbleHMC)
+
+##### LOAD DATA #############################################################################################
+
+load("./nimble_data.rda")
+load("./nimble_constants.rda")
+
+##### MODEL SCRIPT ##########################################################################################
+
+code <- nimbleCode({
+    
+  for (i in 1:n.count){
+    ## LINEAR PROCESS MODEL ##
+    log(lam[i]) <- 
+      beta_in +
+      beta_fa * FA[i] +
+      beta_ea * EA[i] +
+      beta_hp * HP[i] +
+      beta_lt * LT[i] * YR[i] +
+      beta_yr * YR[i] +
+      beta_ltln[route[i]] +
+      beta_ef * EF[i] +
+      eps[i]
+    
+    ### overdispersion
+    eps[i] ~ dnorm(0, sd = sd_ovds)
+    
+    ## LIKELIHOOD ##
+    count[i] ~ dpois(lam[i])
+    sim_count[i] ~ dpois(lam[i])
+  } # i
+  
+  ##################################### PRIORS ##############################################################
+  
+  beta_in ~ dnorm(0, sd = 2)
+  beta_fa ~ dnorm(0, sd = 2)
+  beta_ea ~ dnorm(0, sd = 2)
+  beta_hp ~ dnorm(0, sd = 2)
+  beta_yr ~ dnorm(0, sd = 2)
+  beta_lt ~ dnorm(0, sd = 2)
+  beta_ef ~ dnorm(0, sd = 2)
+  sd_ovds ~ dunif(0, 10)
+  
+  ##################################### SPATIAL GAM EFFECTS #################################################
+  
+  sd_ltln ~ dgamma(1,1)
+  
+  for (k in 1:n.knots) {
+    BX_ltln[k] ~ dnorm(0, sd = sd_ltln)
+  }
+  
+  ##################################### DERIVED / MARGINAL EFFECTS ##########################################
+  for (i in 1:n.route) {
+    beta_ltln[i] <- inprod(BX_ltln[1:n.knots], latlon[i,1:n.knots])
+  }
+  
+  baseline[1:n.route] <- exp(beta_in + beta_ltln[1:n.route])
+  marginal_fa[1:n.count] <- exp(beta_in + beta_fa * FA[1:n.count])
+  marginal_ea[1:n.count] <- exp(beta_in + beta_ea * EA[1:n.count])
+  marginal_yr[1:n.count] <- exp(beta_in + beta_yr * YR[1:n.count] + beta_lt * LT[1:n.count] * YR[1:n.count])
+  marginal_hp[1:n.count] <- exp(beta_in + beta_hp * HP[1:n.count])
+  
+  ### Freeman–Tukey residuals
+  Rn1i[1:n.count] <- pow(sqrt(count[1:n.count]) - sqrt(lam[1:n.count]), 2)
+  Rn2i[1:n.count] <- pow(sqrt(sim_count[1:n.count]) - sqrt(lam[1:n.count]), 2)
+  
+  Rn1 <- sum(Rn1i[1:n.count])
+  Rn2 <- sum(Rn2i[1:n.count])
+  # Bayesian p-value
+  bayes.p <- Rn2 > Rn1
+})
+
+##### MODEL PARAMS ##########################################################################################
+
+params <- c("beta_in","beta_fa","beta_ea","beta_yr","beta_lt","beta_hp","beta_ef","BX_ltln","sd_ltln","sd_ovds",
+            "baseline","marginal_fa","marginal_ea","marginal_yr","marginal_hp","bayes.p","Rn2","Rn1")
+
+inits <- list(
+  beta_in = 4,
+  beta_fa = 0,
+  beta_ea = 0,
+  beta_hp = 0,
+  beta_lt = 0,
+  beta_yr = 0,
+  beta_ef = 0.5,
+  BX_ltln = rep(0, nimble.constants$n.knots),
+  eps = rep(0, nimble.constants$n.count),
+  sd_ovds = 1,
+  sd_ltln = 1,
+  sim_count = round(nimble.data$count * 1.3)
+)
+
+##### CONFIGURE AND RUN MODEL ###############################################################################
+
+Rmodel <- nimbleModel(code, nimble.constants, nimble.data, inits, buildDerivs = T)
+config <- configureHMC(Rmodel, monitors = params)
+Rmcmc <- buildMCMC(config)
+
+Cmodel <- compileNimble(Rmodel)
+Cmcmc <- compileNimble(Rmcmc, project = Rmodel)
+samples <- runMCMC(
+  Cmcmc,
+  nchains = 1,
+  niter = 18000,
+  nburnin= 6000,
+  thin = 2,
+  samplesAsCodaMCMC = T
+)
+
+idx <- length(list.files("./", pattern = "anna_glmnuts_chain"))+1
+assign(paste0("anna_glmnuts_chain", idx), value = samples)
+
+save(list = paste0("anna_glmnuts_chain", idx), file = paste0("./anna_glmnuts_chain", idx, ".rda"))
